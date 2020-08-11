6be6a5eb106512604033cdaf30f112139e8179b5
NguHoaDuy <62531944+NguHoaDuy@users.noreply.github.com>
2020-08-11 09:40:30 +0700

    angular8

---
+++ main/src/app/auction/auction.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AuctionRoutingModule } from './auction-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AuctionRoutingModule
+  ]
+})
+export class AuctionModule { }
