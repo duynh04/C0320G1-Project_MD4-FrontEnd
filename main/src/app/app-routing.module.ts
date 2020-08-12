611850cb538103499737580d17ce7c94d6b1680b
NguHoaDuy <62531944+NguHoaDuy@users.noreply.github.com>
2020-08-12 19:53:57 +0700

    update nth

--- main/src/app/app-routing.module.ts
+++ main/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
 const routes: Routes = [
   {
     path: 'payment', loadChildren: () => import('./payment/payment.module').then(mod => mod.PaymentModule)
-  }
+  },
+  { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
--- main/src/app/app.module.ts
+++ main/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
