872cee55c9d8b6ac7e9b32efe4461c82851810f7
nancynguyen14701 <ngannguyenqn2001@gmail.com>
2020-08-15 21:15:45 +0700

    C-Ngan updated some models Saturday

--- main/src/app/shared/models/product.ts
+++ main/src/app/shared/models/product.ts
@@ -1,5 +1,17 @@
+import { User } from './user';
+import { Category } from './category';
+import { ApprovementStatus } from './approvement-status';
 export interface Product {
-    [prop: string]: any;
-    // mọi người add thêm prop của đối tượng dưới này, 
-    // nếu có người add rồi thì xin bỏ qua. xin cảm ơn.
+   id: number;
+   name: string;
+   initialPrice: number;
+   increasingAmount: number;
+   registerDate: string;
+   startDate: string;
+   endDate: string;
+   approvementStatus: ApprovementStatus;
+   description: string;
+   category: Category;
+   owner: User;
+
 }
