7272149febd08e0a6cacd2ec32122141e7065955
Toan Nguyen <toannguyen.hue92@gmail.com>
2020-08-16 10:56:11 +0700

    Ghi chú cho các request

--- main/src/app/shared/services/cart.service.ts
+++ main/src/app/shared/services/cart.service.ts
@@ -23,22 +23,27 @@ export class CartService {
   constructor(private http: HttpClient) {
   }
 
+  // Lấy thông tin giỏ hàng của một user
   getCart(userId: number): Observable<Cart> {
     return this.http.get<Cart>(this.API + `?userId=${userId}`, this.httpOptions);
   }
 
+  // Cập nhật lại tổng giá tiền khi có sự thay đổi
   updateTotalCost(cartId: number): Observable<number> {
     return this.http.put<number>(this.API, {cartId}, this.httpOptions);
   }
 
+  // Lưu một sản phẩm đấu giá thành công vào giỏ hàng
   saveToCart(cartDetailDTO: CartDetailDTO): Observable<CartDetail> {
     return this.http.post<CartDetail>(this.API + '/cart-detail', cartDetailDTO, this.httpOptions);
   }
 
+  // Thay đổi số lượng của một sản phẩm
   updateItem(cartDetailId: number, quantity: number): Observable<CartDetail> {
     return this.http.put<CartDetail>(this.API + '/cart-detail', {cartDetailId, quantity}, this.httpOptions);
   }
 
+  // Xóa sản phẩm khỏi giỏ hàng
   deleteItem(cartDetailId: number): Observable<CartDetail> {
     return this.http.delete<CartDetail>(this.API + `/cart-detail/${cartDetailId}`, this.httpOptions);
   }
