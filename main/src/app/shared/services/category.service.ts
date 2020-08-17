35d35cf0cfe014120907c448f2a65d9982ba41fc
Tunt82 <tunguyen.engineer@gmail.com>
2020-08-17 09:53:23 +0700

    C-Tu them file: .idea/.gitignore

---
+++ main/src/app/shared/services/category.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Category} from '../../shared/models/category';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService {
+  private readonly API_URL = 'http://localhost:3000/categories';
+  constructor(private http: HttpClient) { }
+
+  httpOption = {
+    headers: new HttpHeaders({
+      'content-Type' : 'application/json'
+    })
+  };
+
+  getCategories(): Observable<Category[]> {
+    return this.http.get<Category[]>(this.API_URL);
+  }
+}
