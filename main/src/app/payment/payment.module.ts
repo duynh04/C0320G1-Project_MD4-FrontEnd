fcac8b6ed9cfbe7b2116b946377c26d00b1ed5df
hung0701 <hung38k14@gmail.com>
2020-08-17 15:42:17 +0700

    BHung import ngx-paypal from payment.module

--- main/src/app/payment/payment.module.ts
+++ main/src/app/payment/payment.module.ts
@@ -1,10 +1,10 @@
+import { NgxPayPalModule } from 'ngx-paypal';
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { ReactiveFormsModule, FormsModule } from "@angular/forms";
 import { PaymentRoutingModule } from './payment-routing.module';
 import { PaymentCenterComponent } from './payment-center/payment-center.component';
 import { DeliveryAddressComponent } from './delivery-address/delivery-address.component';
-
 
 @NgModule({
   declarations: [PaymentCenterComponent, DeliveryAddressComponent],
