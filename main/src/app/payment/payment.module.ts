f6a35d8c9306344be75ce011ac5753089bffb753
NguHoaDuy <62531944+NguHoaDuy@users.noreply.github.com>
2020-08-18 09:20:30 +0700

    udpate payment

--- main/src/app/payment/payment.module.ts
+++ main/src/app/payment/payment.module.ts
@@ -5,9 +5,16 @@ import { ReactiveFormsModule, FormsModule } from "@angular/forms";
 import { PaymentRoutingModule } from './payment-routing.module';
 import { PaymentCenterComponent } from './payment-center/payment-center.component';
 import { DeliveryAddressComponent } from './delivery-address/delivery-address.component';
+import { PaymentOptionComponent } from './payment-option/payment-option.component';
+import { PaymentInvoiceComponent } from './payment-invoice/payment-invoice.component';
 
 @NgModule({
-  declarations: [PaymentCenterComponent, DeliveryAddressComponent],
+  declarations: [
+    PaymentCenterComponent,
+    DeliveryAddressComponent,
+    PaymentOptionComponent,
+    PaymentInvoiceComponent
+  ],
   imports: [
     CommonModule,
     PaymentRoutingModule,
