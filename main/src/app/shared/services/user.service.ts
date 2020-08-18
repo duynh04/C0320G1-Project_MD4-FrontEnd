70d5184865169e541ef8e6ab09316cd37e570b77
HuuHau0909 <huuhau0909@gmail.com>
2020-08-18 22:08:20 +0700

    làm lai

--- main/src/app/shared/services/user.service.ts
+++ main/src/app/shared/services/user.service.ts
@@ -1,25 +1,50 @@
-import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
+import {UserUpdateDto} from '../models/dtos/UserUpdateDto';
+import {User} from './../models/User';
+import {Injectable} from '@angular/core';
+import {HttpHeaders, HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {AbstractControl} from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class UserService {
+  private readonly API_URL_USER = "http://localhost:8080/api/v1";
 
-  private baseUrl = 'http://localhost:8080/api/v1/userList';
-
-  constructor(
-    private http: HttpClient ) { }
-
-  getUserList(): Observable<any> {
-    return this.http.get(`${this.baseUrl}`);
+  constructor(private http: HttpClient) {
   }
 
-  // createUser(user: Object): Observable<Object> {
-  //   return this.http.post(`${this.baseUrl}`, user);
-  // }
-  updateUser(id: number, value: any): Observable<Object> {
-    return this.http.put(`${this.baseUrl}/${id}`, value);
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  }
+
+  //creator: Nguyễn Xuân Hùng
+  getUserById(id): Observable<UserUpdateDto> {
+    return this.http.get<UserUpdateDto>(this.API_URL_USER + id);
+  }
+
+  //creator: Nguyễn Xuân Hùng
+  updateUser(user: UserUpdateDto, id): Observable<UserUpdateDto> {
+    return this.http.put<UserUpdateDto>(this.API_URL_USER + 'update/' + id, JSON.stringify(user), this.httpOptions);
+  }
+
+  //creator: Nguyễn Xuân Hùng
+  comparePassword(c: AbstractControl) {
+    const v = c.value;
+    return (v.newPassword === v.confirmPassword) ? null : {
+      passwordnotmatch: true
+    };
+  }
+
+   getAllUser(): Observable<User[]> {
+    return this.http.get<User[]>(this.API_URL_USER+"/userList");
+  }
+
+  createUser(user: User): Observable<User> {
+    return this.http.post<User>(this.API_URL_USER + "/create", user);
+    // return this.http.post<Employee>(this.API_URL,Employee);
   }
 }
--- main/src/app/user/user-list/user-list.component.ts
+++ main/src/app/user/user-list/user-list.component.ts
@@ -1,79 +1,84 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {User} from "../../shared/models/user";
 import {UserService} from "../../shared/services/user.service";
 import {Router} from "@angular/router";
-
-import {User} from "../../shared/models/user";
 import {Observable} from "rxjs";
-import {FormArray, FormBuilder, FormGroup} from "@angular/forms";
+import {FormArray, FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.css']
-
 })
 export class UserListComponent implements OnInit {
-  // newUser: any = {};
-  addForm: FormGroup;
 
-  rows: FormArray;
-  itemForm: FormGroup;
   users: User[] = [];
-  userList: Observable<User[]>;
-  user: User;
-  i: any;
-  enableEdit = false;
-  enableEditIndex = null;
+  addForm: FormGroup;
+  rows: FormArray;
+
+  @Output()
+  doCreate = new EventEmitter();
 
   constructor(
-    private fb: FormBuilder,
-    private userService: UserService,
-    private route: Router
+    public userService: UserService,
+    private route: Router,
+    private fb: FormBuilder
   ) {
-    this.addForm = this.fb.group()
+    this.addForm = this.fb.group({
+      items: [null, Validators.required],
+      items_value: ['no', Validators.required]
+    });
+    this.rows = this.fb.array([]);
+    // this.addForm= this.fb.group({
+    //   fullname: ["", Validators.required],
+    //   address: ["", Validators.required],
+    //   rate: ["", Validators.required],
+    //   email: ["", Validators.required],
+    //   phoneNumber: ["", Validators.required],
+    //   lastLogin: ["", Validators.required],
+    //   point: ["", Validators.required]
+    // });
   }
 
-  // dynamicArray: Observable<User> = [];
-  // newDynamic: any = {};
-
-
   ngOnInit() {
-    this.userService.getUserList().subscribe(data => {
-      this.users = data
+    this.addForm.addControl('rows', this.rows);
+    this.userService.getAllUser().subscribe(data => {
+      this.users = data;
     });
   }
 
 
-  // addRow(index) {
-  //   this.newUser = {fullname: "", address: "", rate: "", email: "", phoneNumber: "", lastLogin: "", point: ""};
-  //   this.users.push(this.newUser);
-  //
-  //   // this.user = new User();
-  //   // this.gotoList();
-  //   return true;
-  // }
-
-  // deleteRow(index) {
-  //   // if(this.dynamicArray.length ==1) {
-  //   //   // this.toastr.error("Can't delete the row when there is only one row", 'Warning');
-  //   //   return false;
-  //   // } else {
-  //   this.users.splice(index, 1);
-  //   // this.toastr.warning('Row deleted successfully', 'Delete row');
-  //   // return true;
-  // }
-
-  enableEditMethod(e, i) {
-    this.enableEdit = true;
-    this.enableEditIndex = i;
-    console.log(i, e);
+  onAddRow() {
+    this.rows.push(this.createItemFormGroup());
   }
 
-//   saveUser() {
-// this.userService.updateUser(this.id,this.user)
-//   .subscribe(data => console.log(data), error => console.log(error));
-//     this.user = new User();
-//
-//   }
+  createItemFormGroup(): FormGroup {
+    return this.fb.group({
+      fullname: ["", Validators.required],
+      address: ["", Validators.required],
+      rate: ["", Validators.required],
+      email: ["", Validators.required],
+      phoneNumber: ["", Validators.required],
+      lastLogin: ["", Validators.required],
+      point: ["", Validators.required]
+    });
+  }
+
+  onRemoveRow(rowIndex: number) {
+    this.rows.removeAt(rowIndex);
+  }
+
+  // onSubmit() {
+  //   let row = this.addForm.value as User;
+  //   this.userService.createUser(row).subscribe(() => {
+  //     this.doCreate.emit();
+      // this.router.navigateByUrl('/employees');
+    // });
+    // createUser(index: number) {
+    //
+    // }
+  // }
+
+
 
 }
