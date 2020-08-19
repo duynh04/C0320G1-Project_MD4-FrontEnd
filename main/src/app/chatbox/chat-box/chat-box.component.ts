eea78bf0c5261d06c0f0d4b287a85cf5f471a588
unknown <vipcafe1994>
2020-08-19 11:22:54 +0700

    update chatbot

--- main/src/app/chatbox/chat-box/chat-box.component.ts
+++ main/src/app/chatbox/chat-box/chat-box.component.ts
@@ -2,7 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import * as io from 'socket.io-client';
 import {element} from "protractor";
 
-const SOCKET_ENDPOINT = 'localhost:3000';
+const SOCKET_ENDPOINT = 'https://chatbot-facbook.herokuapp.com/';
 
 @Component({
   selector: 'app-chat-box',
@@ -25,12 +25,14 @@ export class ChatBoxComponent implements OnInit {
     this.socket = io(SOCKET_ENDPOINT);
     this.socket.on('message-broadcast', (data: string) => {
       if (data) {
-        const element = document.createElement('li');
+        const element = document.createElement('div');
         element.innerHTML = data;
-        element.style.background = 'white';
-        element.style.padding = '15px 30px';
+        element.style.background = '#0360a5';
+        element.style.padding = '10px 10px';
         element.style.margin = '10px';
-        element.style.borderRadius = '5px';
+        element.style.color = 'white';
+        element.style.overflowY = 'auto';
+        element.style.borderRadius = '8px';
         document.getElementById('message-list').appendChild(element);
       }
     });
@@ -39,14 +41,14 @@ export class ChatBoxComponent implements OnInit {
   SendMessage() {
     this.message ="<span style='color: red ; font-weight: bold'>" + this.guestid + ' : '+"</span>" + this.message;
     this.socket.emit('message', this.message);
-    const element = document.createElement('li');
+    const element = document.createElement('p');
     element.innerHTML = this.message;
     element.style.background = 'black';
-    element.style.padding = '15px 30px';
+    element.style.padding = '10px 10px';
     element.style.margin = '10px';
     element.style.color = 'white';
     element.style.textAlign = 'right';
-    element.style.borderRadius = '5px';
+    element.style.borderRadius = '8px';
     document.getElementById('message-list').appendChild(element);
     this.message = '';
   }
@@ -54,10 +56,10 @@ export class ChatBoxComponent implements OnInit {
   createGusetID() {
     this.guestid = 'Guest' + Math.floor(Math.random() * Math.floor(100));
     this.socket.emit('guest', this.guestid);
-    const  element = document.createElement('p');
+    const  element = document.createElement('span');
     element.innerHTML = this.guestid + ' Join in rom ';
     element.style.color = 'red';
     element.style.fontWeight = 'bold';
-    document.getElementById('message-list').appendChild(element)
+    document.getElementById('message-list').appendChild(element);
   }
 }
--- main/src/assets/js/chatbot.js
+++ main/src/assets/js/chatbot.js
@@ -1,4 +1,6 @@
 let lg = 0;
+let fb = 0;
+
 function loginForm() {
   let b = document.getElementById("loginForm").style.display;
   if (b == "none") {
@@ -34,25 +36,37 @@ function openButton() {
   console.log(lg)
 }
 
-
-function sender() {
-
-  document.getElementById("myForm").style.display = "block";
-  let a = document.getElementById("content-text") ;
-  document.getElementById("print-text").innerHTML = a.value ;
-  document.getElementById('user').hidden=false;
-  console.log(a.value)
-  a.value = ""
-
-}
-
 function logOut() {
 
-  if(confirm("bạn có muốn đăng xuất !!!")==true) {
-    lg = 0 ;
+  if (confirm("bạn có muốn đăng xuất !!!") == true) {
+    lg = 0;
     document.getElementById("myForm").style.display = "none";
-    alert("dăng xuất thành công !!")
-    document.getElementById("print-text").innerHTML = "" ;
-    document.getElementById('user').hidden=true;
+    alert("dăng xuất thành công !!");
+    document.getElementById("print-text").innerHTML = "";
+    document.getElementById('user').hidden = true;
   }
+}
+
+
+window.fbAsyncInit = function () {
+  FB.init({
+    xfbml: true,
+    version: 'v8.0'
+  });
+};
+
+(function (d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s);
+  js.id = id;
+  js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+function loginFB() {
+  document.getElementById('fb-root').style.display = 'block';
+  document.getElementById("draggable").style.display = "none";
+  document.getElementById("loginForm").style.display = "none";
+
 }
