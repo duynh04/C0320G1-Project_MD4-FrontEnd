19dafe8a7decf9732476d9ee615cded372009e4f
thienng5898 <zeusfallen98@gmail.com>
2020-08-20 11:35:18 +0700

    Login update validate, icon

--- main/src/app/user/login/login.component.ts
+++ main/src/app/user/login/login.component.ts
@@ -1,36 +1,51 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {AbstractControl, FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {AuthLoginInfo} from '../../auth/login-info';
 import {AuthJwtService} from '../../auth/auth-jwt.service';
 import {TokenStorageService} from '../../auth/token-storage.service';
 import {Router} from '@angular/router';
-
+declare let $: any;
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
   submitted = false;
   userInfo: AuthLoginInfo;
+  message = '';
   constructor(private auth: AuthJwtService, private fb: FormBuilder,
               private tokenStorage: TokenStorageService, private router: Router) { }
 
   ngOnInit() {
     this.loginForm = this.fb.group({
-      username: ['', [Validators.required]],
-      password: ['', Validators.required]
+      email: ['', [Validators.required, validateWhitespace ,
+        Validators.pattern('^[a-z][a-z0-9_\\.]{2,32}@[a-z0-9]{2,}(\\.[a-z0-9]{2,4}){1,2}$')]],
+      password: ['', [Validators.required]],
+    });
+
+    $('#togglePassword').click(() => {
+      // tslint:disable-next-line:prefer-const
+      let passwordFieldType = $('#password').attr('type');
+      // tslint:disable-next-line:triple-equals
+      if (passwordFieldType == 'password') {
+      $('#password').prop('type', 'text');
+      } else {
+        $('#password').prop('type', 'password');
+      }
+
     });
   }
   onSubmit() {
     this.submitted = true;
-    this.userInfo = new AuthLoginInfo(this.fusername.value, this.fpassword.value);
+    this.userInfo = new AuthLoginInfo(this.femail.value, this.fpassword.value);
     this.login(this.userInfo);
   }
 
-  get fusername() {
-    return this.loginForm.get('username');
+  get femail() {
+    return this.loginForm.get('email');
   }
   get fpassword() {
     return this.loginForm.get('password');
@@ -45,15 +60,38 @@ export class LoginComponent implements OnInit {
         this.tokenStorage.saveUsername(data.accountName);
         // tslint:disable-next-line:triple-equals
         if (this.tokenStorage.getAuthorities().indexOf('ROLE_ADMIN') != -1) {
-          this.router.navigateByUrl('/user/cart');
+          this.router.navigateByUrl('/admin');
+          // tslint:disable-next-line:triple-equals
+        } else  if (this.tokenStorage.getAuthorities().indexOf('ROLE_USER') != -1) {
+          this.router.navigateByUrl('/home');
         }
         console.log(this.tokenStorage.getAuthorities());
       },
       error => {
         console.log('Error ', error);
+        this.message = 'Tài Khoản này không đúng hoặc đã bị khóa';
       }
     );
 
   }
 
 }
+function validateWhitespace(c: AbstractControl) {
+  if (c.value !== '') {
+    const isWhitespace = c.value.trim().length === 0;
+    const isValid = !isWhitespace;
+    return isValid ? null : { whitespace: true };
+  }
+  return null;
+}
+
+// const togglePassword = document.querySelector('##togglePassword');
+// const password = document.querySelector('#password');
+// togglePassword.addEventListener('click', function() {
+//   const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+//   password.setAttribute('type', type);
+//   this.classList.toggle('fa-eye-slash');
+// });
+
+// tslint:disable-next-line:only-arrow-functions
+
