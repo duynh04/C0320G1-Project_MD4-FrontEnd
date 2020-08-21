5c324f252ba5ddd0e2359f193fcc944f9e647b5b
Toan Nguyen <toannguyen.hue92@gmail.com>
2020-08-21 14:22:17 +0700

    Chỉnh sửa header

--- main/src/app/header/header.component.ts
+++ main/src/app/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { TokenStorageService } from '../auth/token-storage.service';
 import { Router, NavigationStart } from '@angular/router';
 import { filter } from 'rxjs/operators';
+import { CartService } from '../shared/services/cart.service';
 
 @Component({
   selector: 'app-header',
@@ -11,14 +12,20 @@ import { filter } from 'rxjs/operators';
 export class HeaderComponent implements OnInit {
 
   email: string;
+  cartCount: number;
 
   constructor(
     private token: TokenStorageService,
+    private cartService: CartService,
     private router: Router) {
     this.router.events
       .pipe(filter(event => event instanceof NavigationStart))
       .subscribe(() => {
         this.email = window.sessionStorage.getItem('AuthUsername');
+        const userId = this.token.getUserId();
+        this.cartService.getCart(userId).subscribe(cart => {
+          this.cartCount = cart.cartDetails.length;
+        }, () => this.cartCount = 0);
       });
   }
 
