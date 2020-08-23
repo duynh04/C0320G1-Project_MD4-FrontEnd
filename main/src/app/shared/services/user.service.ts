adebc81309b9ed86f40dce4a40ab4d969f3e0d46
NguHoaDuy <62531944+NguHoaDuy@users.noreply.github.com>
2020-08-23 21:24:01 +0700

    Mau Team-A

--- main/src/app/shared/services/user.service.ts
+++ main/src/app/shared/services/user.service.ts
@@ -1,9 +1,9 @@
-import {UserUpdateDto} from '../models/dtos/UserUpdateDto';
-import {UserDto} from '../models/dtos/userDto';
-import {Injectable} from '@angular/core';
-import {HttpHeaders, HttpClient} from '@angular/common/http';
-import {Observable} from 'rxjs';
-import {AbstractControl} from '@angular/forms';
+import { UserUpdateDto } from '../models/dtos/UserUpdateDto';
+import { UserDto } from '../models/dtos/userDto';
+import { Injectable } from '@angular/core';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AbstractControl } from '@angular/forms';
 
 
 @Injectable({
@@ -14,8 +14,8 @@ export class UserService {
   private readonly API_URL_USER = 'http://localhost:8080/api/v1/user/';
   constructor(private http: HttpClient) { }
   httpOptions = {
-    headers : new HttpHeaders({
-      'Content-Type' : 'application/json'
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
     })
   };
   // creator: Nguyễn Xuân Hùng
@@ -30,21 +30,21 @@ export class UserService {
   comparePassword(c: AbstractControl) {
     const v = c.value;
     return (v.newPassword === v.confirmPassword) ? null : {
-      passwordnotmatch : true
+      passwordnotmatch: true
     };
   }
   // creator: Trương Khánh Mậu
   createUser(userDto: UserDto): Observable<UserDto> {
-    return  this.http.post<UserDto>(this.API_URL_USER , JSON.stringify(userDto), this.httpOptions);
+    return this.http.post<UserDto>(this.API_URL_USER + 'register', JSON.stringify(userDto), this.httpOptions);
   }
 
   // creator: Trương Khánh Mậu
   checkEmail(email: string): Observable<any> {
-    return  this.http.post<any>(this.API_URL_USER + 'checkEmail' , email, this.httpOptions);
+    return this.http.post<any>(this.API_URL_USER + 'checkEmail', email, this.httpOptions);
   }
 
   // creator: Trương Khánh Mậu
   checkPhone(phoneNumber: string): Observable<any> {
-    return  this.http.post<any>(this.API_URL_USER + 'checkPhone' , phoneNumber, this.httpOptions);
+    return this.http.post<any>(this.API_URL_USER + 'checkPhone', phoneNumber, this.httpOptions);
   }
 }
