9714c9a8b6a45365ec09e901f608ad62590fd1f4
win811 <cuongwin811@gmail.com>
2020-08-24 11:53:52 +0700

    merge với chị B-Thao

--- main/src/app/admin/admin-routing.module.ts
+++ main/src/app/admin/admin-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DealManageComponent } from './deal-manage/deal-manage.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: 'deal-management',
+        component: DealManageComponent,
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- main/src/app/admin/admin.module.ts
+++ main/src/app/admin/admin.module.ts
@@ -1,12 +1,16 @@
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
+import { DealManageComponent } from './deal-manage/deal-manage.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [DealManageComponent],
   imports: [
+    FormsModule,
+    ReactiveFormsModule,
     CommonModule,
     AdminRoutingModule
   ]
